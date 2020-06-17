d97c593cfb4f7203ad5e5f4ab9f3b0c261097f7f
ChristianLievrouw <celievrouw1@gmail.com>
2020-06-17 13:09:08 -0500

    added ordered list of fav movies/ unordered list of work and school history

--- js/app.js
+++ js/app.js
@@ -2,54 +2,57 @@
 //first thing to do
 console.log('This file is connected.');
 
-var userName = prompt('What is your name?!');
-// console.log(userName);
+// var userName = prompt('What is your name?!');
+// // console.log(userName);
 
-alert('Hello ' + userName);
+// alert('Hello ' + userName);
 
-var iWasBorn = prompt('Was I born in Charles City, IA? yes/no').toLowerCase();
-// console.log(iWasBorn);
+// var iWasBorn = prompt('Was I born in Charles City, IA? yes/no').toLowerCase();
+// // console.log(iWasBorn);
 
-if(iWasBorn === 'no'){
-  alert('You\'re right ' + userName);
-} else {
-  alert('Nope! good try though, I was born in Mason City, IA');
-}
+// if(iWasBorn === 'no'){
+//   alert('You\'re right ' + userName);
+// } else {
+//   alert('Nope! good try though, I was born in Mason City, IA');
+// }
 
-var myChild = prompt('Do I have a daughter? yes/no').toLowerCase();
-// console.log(myChild);
+// var myChild = prompt('Do I have a daughter? yes/no').toLowerCase();
+// // console.log(myChild);
 
-if(myChild === 'no'){
-  alert('You\'re right! I have a son and his name is Liam');
-} else {
-  alert('Nope! good try though!');
-}
+// if(myChild === 'no'){
+//   alert('You\'re right! I have a son and his name is Liam');
+// } else {
+//   alert('Nope! good try though!');
+// }
 
-var myHobby = prompt('Is one of my favorite hobbies drawing? yes/no').toLowerCase();
-// console.log(myHobby);
+// var myHobby = prompt('Is one of my favorite hobbies drawing? yes/no').toLowerCase();
+// // console.log(myHobby);
 
-if(myHobby === 'yes'){
-  alert('You\'re right ' + userName + ' I love to draw!');
-} else {
-  alert('Drawing actually is one of my favorite hobbies ' + userName );
-}
+// if(myHobby === 'yes'){
+//   alert('You\'re right ' + userName + ' I love to draw!');
+// } else {
+//   alert('Drawing actually is one of my favorite hobbies ' + userName );
+// }
 
-var dreamJob = prompt('Is my dream job to be a software developer for Apple? yes/no').toLowerCase();
-// console.log(dreamJob);
+// var dreamJob = prompt('Is my dream job to be a software developer for Apple? yes/no').toLowerCase();
+// // console.log(dreamJob);
 
-if(dreamJob === 'yes'){
-  alert('You\'re correct ' + userName + '!! I could not think of anywhere I would wanna work more');
-} else {
-  alert('Nope! good try though, I was born in Mason City, IA');
-}
+// if(dreamJob === 'yes'){
+//   alert('You\'re correct ' + userName + '!! I could not think of anywhere I would wanna work more');
+// } else {
+//   alert('Nope! good try though, I was born in Mason City, IA');
+// }
 
-var homeTown = prompt('Was I raised in Charles City, IA? yes/no').toLowerCase();
-// console.log(homeTown);
+// var homeTown = prompt('Was I raised in Charles City, IA? yes/no').toLowerCase();
+// // console.log(homeTown);
 
-if(homeTown === 'yes'){
-  alert('You\'re right ' + userName + ', I grew up in Charles City, IA. That is about two hours away from Cedar Rapids, IA');
-} else {
-  alert('Nope! good try though, I was raised in Charles City, IA');
-}
+// if(homeTown === 'yes'){
+//   alert('You\'re right ' + userName + ', I grew up in Charles City, IA. That is about two hours away from Cedar Rapids, IA');
+// } else {
+//   alert('Nope! good try though, I was raised in Charles City, IA');
+// }
 
-alert('Thank you ' + userName + ' for taking the time to get to know me a little more!');
+// alert('Thank you ' + userName + ' for taking the time to get to know me a little more!');
+
+var userPoints = 0;
+
