d5242b0c924aaab126a36530345d2be7c9e0bdfb
ChristianLievrouw <celievrouw1@gmail.com>
2020-06-17 17:44:05 -0500

    add lab

--- js/app.js
+++ js/app.js
@@ -2,15 +2,15 @@
 //first thing to do
 console.log('This file is connected.');
 
-var userPoints = 0;
+// var userPoints = 0;
 
 // var userName = prompt('What is your name?!');
-// // console.log(userName);
+// console.log(userName);
 
 // alert('Hello ' + userName);
 
 // var iWasBorn = prompt('Was I born in Charles City, IA? yes/no').toLowerCase();
-// // console.log(iWasBorn);
+// console.log(iWasBorn);
 
 // if(iWasBorn === 'no'){
 //   alert('You\'re right ' + userName);
@@ -19,7 +19,7 @@ var userPoints = 0;
 // }
 
 // var myChild = prompt('Do I have a daughter? yes/no').toLowerCase();
-// // console.log(myChild);
+// console.log(myChild);
 
 // if(myChild === 'no'){
 //   alert('You\'re right! I have a son and his name is Liam');
@@ -28,7 +28,7 @@ var userPoints = 0;
 // }
 
 // var myHobby = prompt('Is one of my favorite hobbies drawing? yes/no').toLowerCase();
-// // console.log(myHobby);
+// console.log(myHobby);
 
 // if(myHobby === 'yes'){
 //   alert('You\'re right ' + userName + ' I love to draw!');
@@ -37,7 +37,7 @@ var userPoints = 0;
 // }
 
 // var dreamJob = prompt('Is my dream job to be a software developer for Apple? yes/no').toLowerCase();
-// // console.log(dreamJob);
+// console.log(dreamJob);
 
 // if(dreamJob === 'yes'){
 //   alert('You\'re correct ' + userName + '!! I could not think of anywhere I would wanna work more');
@@ -46,7 +46,7 @@ var userPoints = 0;
 // }
 
 // var homeTown = prompt('Was I raised in Charles City, IA? yes/no').toLowerCase();
-// // console.log(homeTown);
+// console.log(homeTown);
 
 // if(homeTown === 'yes'){
 //   alert('You\'re right ' + userName + ', I grew up in Charles City, IA. That is about two hours away from Cedar Rapids, IA');
@@ -55,23 +55,38 @@ var userPoints = 0;
 // }
 
 // alert('Thank you ' + userName + ' for taking the time to get to know me a little more!');
-var userChance = 0;
-var myPick = 3;
+// var userChance = 0;
+// var myPick = 3;
 
-while(userChance < 4){
-  var userNumber = prompt('Choose a number between 1 and 10!');
-  userNumber = Number(userNumber);
-  userChance += 1;
-  if(userChance === 4){
-    alert('You are all of of chances');
-  }else if(userNumber === myPick){
-    alert('Way to go!!');
-    userPoints += 1;
-    break;
-  }else if(userNumber < myPick){
-    alert('Too low!!');
-  }else if(userNumber > myPick){
-    alert('Too high');
+// while(userChance < 4){
+//   var userNumber = prompt('Choose a number between 1 and 10!');
+//   userNumber = Number(userNumber);
+//   userChance += 1;
+//   if(userChance === 4){
+//     alert('You are all of of chances');
+//   }else if(userNumber === myPick){
+//     alert('Way to go!!');
+//     userPoints += 1;
+//     break;
+//   }else if(userNumber < myPick){
+//     alert('Too low!!');
+//   }else if(userNumber > myPick){
+//     alert('Too high');
+//   }
+// }
+var guesses = 0;
+var favMovies = ['Black Hawk Down', 'Ready Player One', 'Rubber', 'Platform', 'Zero Dark Thirty'];
+console.log(favMovies);
+
+var movieAnswer = prompt('Guess what five of my favorite movies are?');
+while(guesses < 7){
+  for (var i = 0; i < favMovies.length; i++) {
+    if (movieAnswer === favMovies[i]) {
+      alert('Awesome you know me so well!');
+      break;
+    }if(movieAnswer === favMovies[i]){
+      alert('Please try again');
+    }
   }
 }
 
